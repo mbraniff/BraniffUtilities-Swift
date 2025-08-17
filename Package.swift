e174adfe92f45137cbec07e401e74b7ecfb6bcb5
Matt Braniff <mbraniff@rocketmail.com>
2025-08-17 01:47:14 -0600

    Moved macros to one package

--- Package.swift
+++ Package.swift
@@ -1,25 +1,30 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
     name: "BraniffUtilities",
-    platforms: [.iOS(.v15), .macOS(.v13)],
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v13)   // macros run on macOS at build time
+    ],
     products: [
         .library(
             name: "BraniffMacros",
-            targets: ["BraniffMacros"])
+            targets: ["BraniffMacros"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", .upToNextMajor(from: "601.0.1")),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1")
     ],
     targets: [
-        .macro(name: "BraniffMacrosImpl", dependencies: [
-            .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-            .product(name: "SwiftSyntaxBuilder", package: "swift-syntax")
-        ]),
-        .target(name: "BraniffMacros", dependencies: ["BraniffMacrosImpl"])
+        .macro(
+            name: "BraniffMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        )
     ]
 )
--- Sources/BraniffMacros/MacroPlugin.swift
+++ Sources/BraniffMacros/MacroPlugin.swift
@@ -5,8 +5,12 @@
 //  Created by Matthew Braniff on 8/17/25.
 //
 
-@attached(member, names: named(CodingKeys))
-public macro AutoCodingKeys() = #externalMacro(
-    module: "BraniffMacrosImpl",
-    type: "CodingKeysMacro"
-)
+import SwiftCompilerPlugin
+import SwiftSyntaxMacros
+
+@main
+struct BraniffMacros: CompilerPlugin {
+    let providingMacros: [any Macro.Type] = [
+        CodingKeysMacro.self
+    ]
+}
